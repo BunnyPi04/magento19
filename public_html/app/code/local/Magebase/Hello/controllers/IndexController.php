ee6c80129871c856c0bb8f769fa30eda3b268114
Hang Pham <hangpt248@gmail.com>
2017-10-10 17:09:56 +0700

    search complete

--- public_html/app/code/local/Magebase/Hello/controllers/IndexController.php
+++ public_html/app/code/local/Magebase/Hello/controllers/IndexController.php
@@ -16,14 +16,93 @@ class Magebase_Hello_IndexController extends Mage_Core_Controller_Front_Action
             ->getCollection()
             ->addAttributeToSelect('*')
             ->setOrder('entity_id', 'DESC')
-            ->setPageSize(1);
-        $arr_products = array();
+            ->setPageSize(100);
+        $arr_customers = array();
         foreach ($customer as $ob)
+        {
+            $arr_customers[]= $ob
+                ->toArray(array());
+            //->toArray();
+        }
+        return $this->getResponse()
+            ->setHeader('Content-type', 'application/json')
+            ->setHeader('Access-Control-Allow-Origin', '*')
+            ->setBody(json_encode($arr_customers));
+    }
+
+    public function productAction()
+    {
+        $product = Mage::getModel('catalog/product')
+            ->getCollection()
+            ->addAttributeToSelect('*')
+            ->setOrder('entity_id', 'DESC')
+            ->setPageSize(100);
+        $arr_products = array();
+        foreach ($product as $ob)
         {
             $arr_products[]= $ob
                 ->toArray(array());
             //->toArray();
         }
-        return $this->getResponse()->setBody(json_encode($arr_products));
+        return $this->getResponse()
+            ->setHeader('Content-type', 'application/json')
+            ->setHeader('Access-Control-Allow-Origin', '*')
+            ->setBody(json_encode($arr_products));
     }
+    public function paymentAction()
+    {
+        $payment = Mage::getModel('payment/config')->getAllMethods();
+
+        foreach ($payment as $paymentCode=>$paymentModel) {
+            $paymentTitle = Mage::getStoreConfig('payment/'.$paymentCode.'/title');
+            $paymentStatus = Mage::getStoreConfig('payment/'.$paymentCode.'/active');
+            $methods[] = array(
+                'title'   => $paymentTitle,
+                'code' => $paymentCode,
+                'status' => $paymentStatus
+            );
+        }
+        return $this->getResponse()
+            ->setHeader('Content-type', 'application/json')
+            ->setHeader('Access-Control-Allow-Origin', '*')
+            ->setBody(json_encode($methods));
+    }
+    public function shippingAction()
+    {
+        $shipping = Mage::getModel('shipping/config')->getAllCarriers();
+        $arr_shipping = array();
+
+        foreach ($shipping as $shippingCode=>$shippingModel) {
+            $shippingTitle = Mage::getStoreConfig('carriers/'.$shippingCode.'/title');
+            $shippingStatus = Mage::getStoreConfig('carriers/'.$shippingCode.'/active');
+            $methods[] = array(
+                'title'   => $shippingTitle,
+                'code' => $shippingCode,
+                'status' => $shippingStatus
+            );
+        }
+        return $this->getResponse()
+            ->setHeader('Content-type', 'application/json')
+            ->setHeader('Access-Control-Allow-Origin', '*')
+            ->setBody(json_encode($methods));
+    }
+    public function searchproductAction()
+    {   $search = $_GET["search"];
+//        $productIds = array('msj006c-Red-M', 'wbk012c-Royal Blue-L');
+        $products = Mage::getModel('catalog/product')
+            ->getCollection()
+            ->addAttributeToFilter('name', array('like' => "%$search%"))
+            ->addAttributeToSelect(array('name','description','price','small_image','weight'))
+            ->load();
+        $arr_search = array();
+        foreach($products as $product){
+            $arr_search[]= $product
+                ->toArray(array());
+        }
+        return $this->getResponse()
+            ->setHeader('Content-type', 'application/json')
+            ->setHeader('Access-Control-Allow-Origin', '*')
+            ->setBody(json_encode($arr_search));
+    }
+
 }
---
+++ public_html/app/code/local/Magebase/Helloworld/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hangpt
+ * Date: 05/10/2017
+ * Time: 17:15
+ */
+class Magebase_Helloworld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "ddddd";
+    }
+}
