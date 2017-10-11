b84ab32ebb2ff244a6c4b615e05ad2d95c18285d
Hang Pham <hangpt248@gmail.com>
2017-10-11 15:50:53 +0700

    search show product done

--- public_html/app/code/local/Magebase/Hello/controllers/IndexController.php
+++ public_html/app/code/local/Magebase/Hello/controllers/IndexController.php
@@ -40,10 +40,18 @@ class Magebase_Hello_IndexController extends Mage_Core_Controller_Front_Action
         $arr_products = array();
         foreach ($product as $ob)
         {
-            $arr_products[]= $ob
-                ->toArray(array());
-            //->toArray();
-        }
+            $arr_products[] = array(
+                'sku'=>$ob->getSKU(),
+                'name'=>$ob->getName(),
+                'type_id'=>$ob->getType_id(),
+                'description'=>$ob->getDescription(),
+                'color'=>$ob->getColor(),
+                'fit'=>$ob->getFit(),
+                'size'=>$ob->getSize(),
+                'price'=>$ob->getPrice(),
+                'weight'=>$ob->getWeight(),
+                'img'=>Mage::getModel('catalog/product_media_config')->getMediaUrl($ob->getData("small_image"))
+            );        }
         return $this->getResponse()
             ->setHeader('Content-type', 'application/json')
             ->setHeader('Access-Control-Allow-Origin', '*')
@@ -87,17 +95,31 @@ class Magebase_Hello_IndexController extends Mage_Core_Controller_Front_Action
             ->setBody(json_encode($methods));
     }
     public function searchproductAction()
-    {   $search = $_GET["search"];
-//        $productIds = array('msj006c-Red-M', 'wbk012c-Royal Blue-L');
+    {   $search = $_GET["search"];http://magento1.dev/index.php/hello/index/product
+//        $productSku = array('msj006c-Red-M', 'wbk012c-Royal Blue-L');
         $products = Mage::getModel('catalog/product')
             ->getCollection()
             ->addAttributeToFilter('name', array('like' => "%$search%"))
-            ->addAttributeToSelect(array('name','description','price','small_image','weight'))
+            ->addAttributeToSelect('*')
+//            ->addAttributeToSelect(array('sku','name','description','price','small_image','weight'))
             ->load();
         $arr_search = array();
+//        $images = array();
         foreach($products as $product){
-            $arr_search[]= $product
-                ->toArray(array());
+            $arr_search[] = array(
+                'sku'=>$product->getSKU(),
+                'name'=>$product->getName(),
+                'description'=>$product->getDescription(),
+                'size'=>$product->getSize(),
+                'price'=>$product->getPrice(),
+                'weight'=>$product->getWeight(),
+                'img'=>Mage::getModel('catalog/product_media_config')->getMediaUrl($product->getData("small_image"))
+        );
+
+//            $arr_search[]= $product
+//                ->toArray(array());
+//            $images_obj = Mage::helper('catalog/image')->init($product, 'small_image');
+//            $images[] = (string)$images_obj;
         }
         return $this->getResponse()
             ->setHeader('Content-type', 'application/json')
